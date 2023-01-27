481d732e0780e6671b4587d92743f3b8b37f8656
Anuz <ceanuz66@gmail.com>
2023-01-27 12:51:03 +0600

    update routing

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router'
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private routes: Router) { }
+
+  ngOnInit() {
+  }
+  onLoadServers(id: number){
+    this.routes.navigate(['servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading' }) // navigate
+  }
+}
